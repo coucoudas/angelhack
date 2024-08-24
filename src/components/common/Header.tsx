b96a237fe6fa9bc726ef6aab532ef244485b481b
minhyeong <minhyeong.dev@gmail.com>
2024-08-24 17:09:13 +0900

    [Feat] resize header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,28 +1,85 @@
+import Svg from "@/asset/SVG";
 import useSignStore from "@/store/signStore";
-import { cn } from "@coucoudas/ui";
+import { Button, cn } from "@coucoudas/ui";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
   const router = useNavigate();
-  const { signOut } = useSignStore();
+  const { user, signOut } = useSignStore();
   const container = {
     positions: "absolute top-0 left-0",
-    displays: "flex items-center justify-between ",
-    sizes: "w-full h-12",
+    displays: "flex justify-center",
+    sizes: "w-full h-14",
     backgrounds: "bg-white",
-    boundaries: "px-4",
   };
   const block = {
-    sizes: "w-full h-12",
+    sizes: "w-full h-14",
+  };
+  const body = {
+    displays: "flex items-center justify-between ",
+    sizes: "w-full max-w-[1020px] h-full",
   };
   return (
     <>
       <div className={cn(block)} />
       <div className={cn(container)}>
-        <div>COUCOUDAS</div>
-        <div className="flex gap-x-4">
-          <button onClick={() => router("/mypage")}>마이페이지</button>
-          <button onClick={() => signOut()}>로그아웃</button>
+        <div className={cn(body)}>
+          <Svg.Logo.Coucoudas />
+          <div className="flex gap-x-2">
+            {!user ? (
+              <Button
+                title="로그인"
+                onClick={() => router("/sign-in")}
+                option={{
+                  width: "w-12",
+                  height: "h-6",
+                  background: "bg-gray-black",
+                  text: "text-white text-[12px]",
+                }}
+              />
+            ) : (
+              <div className="flex gap-x-2">
+                <div className="flex text-gray-black">
+                  <div className="font-pretendard-bold">{user.name}</div>
+                  <div>님</div>
+                </div>
+                <Button
+                  title="로그아웃"
+                  onClick={async () => {
+                    alert("로그아웃 되었습니다.");
+                    await signOut();
+                  }}
+                  option={{
+                    width: "w-[58px]",
+                    height: "h-6",
+                    background: "bg-transparent",
+                    text: "text-gray-black text-[12px]",
+                    boundaries: "border border-gray-300 rounded-[6px]",
+                  }}
+                />
+              </div>
+            )}
+            {/* <Button
+              title="회원가입"
+              onClick={() => signOut()}
+              option={{
+                width: "w-12",
+                height: "h-6",
+                background: "bg-transparent",
+                text: "text-gray-black text-[12px]",
+              }}
+            /> */}
+            <Button
+              title="고객센터"
+              onClick={() => router("/supports")}
+              option={{
+                width: "w-12",
+                height: "h-6",
+                background: "bg-transparent",
+                text: "text-gray-black text-[12px]",
+              }}
+            />
+          </div>
         </div>
       </div>
     </>
