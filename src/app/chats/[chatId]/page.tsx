55ff0173e7b22c37b83e766c80492bec8d8b4b88
minhyeong <minhyeong.dev@gmail.com>
2024-08-25 06:36:53 +0900

    [Feat] chat process

--- src/app/chats/[chatId]/page.tsx
+++ src/app/chats/[chatId]/page.tsx
@@ -1,6 +1,133 @@
+import { ItemBox } from "@/app/items/page";
+import { chatApi } from "@/connection";
+import useChat from "@/hook/useChat";
+import useItem from "@/hook/useItem";
+import useSign from "@/hook/useSign";
+import { cn } from "@coucoudas/ui";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import moment from "moment-timezone";
+import { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ChatPage() {
   const { chatId } = useParams();
-  return <div>{chatId}</div>;
+  const { user } = useSign();
+  const { item } = useItem();
+  const { findByChatId } = useChat();
+  const chatInfo = findByChatId(Number(chatId));
+  const [chat, setChat] = useState<string>("");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const inputBox = {
+    sizes: "w-full h-[56px]",
+    boundaries: "rounded-full",
+    backgrounds: "bg-gray-50",
+    styles: "focus:outline-none pl-6.5",
+  };
+  const { mutate: postChat, isSuccess } = useMutation({
+    mutationKey: ["postChat"],
+    mutationFn: () =>
+      chatApi.chat.post({
+        room_id: Number(chatId),
+        sender_id: Number(user?.id),
+        content: chat,
+      }),
+  });
+  const { data: chatData } = useQuery({
+    enabled: !!user,
+    queryKey: ["getChatRooms", isSuccess],
+    queryFn: () => chatApi.chat.get(Number(chatId)),
+  });
+  const handleEnterPress = (event: KeyboardEvent) => {
+    if (event.key === "Enter") postChat();
+  };
+  useEffect(() => {
+    const inputElement = inputRef.current;
+    if (inputElement) {
+      inputElement.addEventListener("keydown", handleEnterPress);
+    }
+    return () => {
+      if (inputElement) {
+        inputElement.removeEventListener("keydown", handleEnterPress);
+      }
+    };
+  }, []);
+  useEffect(() => {
+    if (isSuccess) {
+      setChat("");
+    }
+  }, [isSuccess]);
+  const nowItem = item(Number(chatInfo?.item_id));
+  return (
+    <div className="relative min-h-screen px-3.5 flex flex-col gap-y-5.5">
+      <div className="flex flex-col items-center w-full">
+        {nowItem && (
+          <button
+            onClick={() => {
+              location.href = `/items/${nowItem.id}`;
+            }}
+            className="flex w-full justify-center bg-white"
+          >
+            <div className="w-[320px] border-2">
+              <ItemBox item={nowItem} />
+            </div>
+          </button>
+        )}
+      </div>
+      {chatData?.results
+        ?.sort((a, b) => {
+          return (
+            moment(b.created_at_at).unix() - moment(a.created_at_at).unix()
+          );
+        })
+        .slice(0, 5)
+        .sort((a, b) => {
+          return (
+            moment(a.created_at_at).unix() - moment(b.created_at_at).unix()
+          );
+        })
+        .map((chat) => (
+          <ChatBox
+            isMe={chat.member_id === user?.id}
+            message={chat.content}
+            createdAt={moment(chat.created_at_at).unix()}
+          />
+        ))}
+      <div className="fixed bottom-16 left-0 flex items-center w-full h-[88px] px-2 bg-white">
+        <input
+          ref={inputRef}
+          className={cn(inputBox)}
+          placeholder="채팅을 입력하세요."
+          value={chat}
+          onChange={(e) => setChat(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
+function ChatBox({
+  isMe = false,
+  createdAt,
+  message,
+}: {
+  message: string;
+  createdAt: number;
+  isMe?: boolean;
+}) {
+  if (isMe)
+    return (
+      <div className="flex flex-col items-end">
+        <div className="ml-auto rounded-l-xl w-full max-w-80 rounded-t-xl p-4 bg-[#D0EBFF]">
+          {message}
+        </div>
+        <div>{moment(createdAt).format("hh:mm A")}</div>
+      </div>
+    );
+  return (
+    <div>
+      <div className="mr-auto rounded-r-xl rounded-t-xl w-full max-w-80 p-4 border border-gray-300 ">
+        {message}
+      </div>
+      {moment(createdAt).format("YYYY-MM-DD HH:mm")}
+    </div>
+  );
 }
--- src/app/chats/page.tsx
+++ src/app/chats/page.tsx
@@ -1,31 +1,90 @@
-import { cn } from "@coucoudas/ui";
+import { cn, OnClick } from "@coucoudas/ui";
+import { useEffect } from "react";
+import moment from "moment-timezone";
+import useUser from "@/hook/useUser";
+import { images } from "@/asset/Images";
+import { useNavigate } from "react-router-dom";
+import useChat from "@/hook/useChat";
+import useSign from "@/hook/useSign";
 
 export default function ChatsPage() {
-  const layout = {
-    displays: "flex flex-col",
-    sizes: "w-full max-w-[456px] min-h-screen",
-    backgrounds: "bg-white",
-  };
+  const router = useNavigate();
+  const { user } = useSign();
+  const { findByUserId } = useUser();
+  const { chatsData } = useChat();
+  useEffect(() => {
+    if (!user) {
+      alert("로그인이 필요합니다.");
+      location.href = "/sign-in";
+    }
+  }, [user]);
   return (
-    <div className={cn(layout)}>
-      <div>채팅방</div>
+    <div className="min-h-screen w-full">
+      <div className="flex items-center h-[56px] px-2 w-full">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+        >
+          <path
+            d="M15 5L8 12L15 19"
+            stroke="#868E96"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+        <div className="text-[22px]">리뷰어 채팅</div>
+      </div>
+      {chatsData?.results
+        ?.sort((a, b) => {
+          return moment(b.updated_at).unix() - moment(a.updated_at).unix();
+        })
+        .map((result) => (
+          <ChatBox
+            onClick={() => router(`/chats/${result.id}`)}
+            name={findByUserId(result.receiver_id)?.name ?? "김쿠팡"}
+            message="마지막으로 온 메시지"
+            createdAt={moment(result.updated_at).unix()}
+          />
+        ))}
     </div>
   );
 }
 
-function ChatBox() {
+function ChatBox({
+  onClick,
+  name,
+  message,
+  createdAt,
+}: {
+  onClick: OnClick;
+  name: string;
+  message: string;
+  createdAt: number;
+}) {
   const container = {
     sizes: "w-full h-[68px]",
     boundaries: "px-5 py-3.5",
   };
+
+  const profile = Math.floor(Math.random() * images.length);
   return (
-    <div className={cn(container)}>
-      <div className="flex gap-x-2">
-        <div className="rounded-full w-10 h-10 bg-black"></div>
-        <div>
-          <div>{}</div>
+    <button onClick={onClick} className={cn(container)}>
+      <div className="flex justify-between">
+        <div className="flex gap-x-2">
+          <div className="w-10 h-10 overflow-hidden rounded-full">
+            <img src={`/images/users/${images[profile]}`} />
+          </div>
+          <div>
+            <div>{name}</div>
+            <div>{message}</div>
+          </div>
         </div>
+        <div>{moment.unix(createdAt).format("오후 HH:mm")}</div>
       </div>
-    </div>
+    </button>
   );
 }
