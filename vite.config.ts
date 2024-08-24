4faed676b8249bbcec4491ab370129952d846343
minhyeong <minhyeong.dev@gmail.com>
2024-08-24 10:23:08 +0900

    [Chore] check env mode

--- vite.config.ts
+++ vite.config.ts
@@ -3,22 +3,20 @@ import react from "@vitejs/plugin-react-swc";
 import mkcert from "vite-plugin-mkcert";
 
 // https://vitejs.dev/config/
+
 const devConfig = (mode: string) =>
   mode === "development" && {
     server: {
-      // host: "local.domain.co.kr", // 인증서 발급을 위한 도메인
       host: "0.0.0.0",
       port: 5173,
     },
   };
 export default ({ mode }: ConfigEnv) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  console.log(`running ${mode} mode!`);
   return defineConfig({
     ...devConfig(mode),
-    plugins: [
-      react(),
-      // mode === "development" && mkcert()
-    ],
+    plugins: [react(), mode === "development" && mkcert()],
     resolve: {
       alias: [
         { find: "@components", replacement: "/src/components" },
