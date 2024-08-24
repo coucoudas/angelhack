6dd52394f146e19f83a4c4f9371e7a161a401753
minhyeong <minhyeong.dev@gmail.com>
2024-08-24 16:00:30 +0900

    [Feat] iframe to coupang

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,26 @@
-import { useDelay } from "@/hook";
+import Header from "@/components/common/Header";
+import useSignStore from "@/store/signStore";
+import { cn } from "@coucoudas/ui";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function RootPage() {
-  const { nowTime } = useDelay({
-    callBack: () => console.log("done"),
-  });
-  return <div>{nowTime}</div>;
+  const router = useNavigate();
+  const { user } = useSignStore();
+
+  useEffect(() => {
+    if (!user) {
+      alert("로그인이 필요합니다.");
+      router("/sign-in");
+    }
+  }, [user]);
+  const layout = {
+    sizes: "w-full min-h-screen",
+  };
+  return (
+    <div className={cn(layout)}>
+      <Header />
+      <iframe src="https://www.coupang.com" className={cn(layout)} />
+    </div>
+  );
 }
