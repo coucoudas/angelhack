971ed16ebd1d9c4d0e1ce3a2caa813b5b7725be0
minhyeong <minhyeong.dev@gmail.com>
2024-08-24 16:58:17 +0900

    [Feat] routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,21 +7,26 @@ import SignUpPage from "./sign-up/page";
 import FindUsername from "./find/username/page";
 import FindPassword from "./find/password/page";
 import MyPage from "./mypage/page";
+import RootLayout from "./layout";
+import SupportPage from "./supports/page";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<RootPage />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/sign-in" element={<SignInPage />} />
-        <Route path="/sign-up" element={<SignUpPage />} />
-        <Route path="/find">
-          <Route path="username" element={<FindUsername />} />
-          <Route path="password" element={<FindPassword />} />
+        <Route path="/" element={<RootLayout />}>
+          <Route index element={<RootPage />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/sign-in" element={<SignInPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
+          <Route path="/find">
+            <Route path="username" element={<FindUsername />} />
+            <Route path="password" element={<FindPassword />} />
+          </Route>
+          <Route path="/mypage" element={<MyPage />} />
+          <Route path="/supports" element={<SupportPage />} />
+          <Route path="*" element={<_404 />} />
         </Route>
-        <Route path="/mypage" element={<MyPage />} />
-        <Route path="*" element={<_404 />} />
       </Routes>
     </BrowserRouter>
   );
