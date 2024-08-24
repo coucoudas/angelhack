29fff7618be162269e3fa3f5f090cd9c8f3761a3
minhyeong <minhyeong.dev@gmail.com>
2024-08-25 04:00:38 +0900

    [Refactor]

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import FloatButton from "@/components/chat/FloatButton";
 import Header from "@/components/Header";
 import useSignStore from "@/store/signStore";
 import { cn } from "@coucoudas/ui";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function RootPage() {
@@ -28,7 +27,10 @@ export default function RootPage() {
     <div>
       <Header />
       <iframe src="https://www.coupang.com" className={cn(layout)} />
-      <FloatButton flag={true} />
+      <div
+        className="fixed top-14 left-0 h-screen w-full"
+        onClick={() => router("/items")}
+      />
     </div>
   );
 }
