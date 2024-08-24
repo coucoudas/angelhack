d392ef038f4611c68e36fedf9535001353e4eb5c
minhyeong <minhyeong.dev@gmail.com>
2024-08-25 03:59:22 +0900

    [Feat] mypage

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,11 +1,13 @@
+import useItem from "@/hook/useItem";
 import useSign from "@/hook/useSign";
 import { cn } from "@coucoudas/ui";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-
+import moment from "moment-timezone";
 export default function MyPage() {
   const router = useNavigate();
   const { user } = useSign();
+  const { usedItems } = useItem();
 
   useEffect(() => {
     if (!user) {
@@ -29,6 +31,14 @@ export default function MyPage() {
         <div>{user?.username}</div>
         <div>{user?.email}</div>
         <div>{user?.password}</div>
+        {usedItems.map((item) => (
+          <div key={item.id}>
+            <div>{item.id}</div>
+            <div>{item.itemId}</div>
+            <div>{item.name}</div>
+            <div>{moment.unix(item.createdAt).format("YYYY-MM-DD")}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
